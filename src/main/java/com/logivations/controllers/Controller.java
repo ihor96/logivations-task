79c1fb7a27010cfef812170b280b4f24ad3dc223
ihor96 <goha1996@ukr.net>
2019-06-24 14:07:14 +0200

    +

--- src/main/java/com/logivations/controllers/Controller.java
+++ src/main/java/com/logivations/controllers/Controller.java
@@ -1,6 +1,7 @@
 package com.logivations.controllers;
 
 import com.logivations.businessLogics.BusinessLogic;
+import com.logivations.helpers.DataBase;
 import com.logivations.models.Case;
 import com.logivations.models.Order;
 import java.util.List;
@@ -9,5 +10,9 @@ public class Controller {
 
    public List<Case> checkAndPutProductsInCase(List<Case> cases, List<Order> orders) {
         return new BusinessLogic().checkBySizeParameters(new BusinessLogic().checkByVolume(cases, orders));
+    }
+
+    public<T> void createObjectInDataBase(T object){
+       DataBase.createObjectInDB(object);
     }
 }
